dafc726ec83c6ab2746edd8c21dc14d8d1a13e64
sajjad-salam <sajjad.salam.teama@gmail.com>
2023-05-31 20:36:17 +0300

    #1

--- #1.py
+++ #1.py
@@ -1,1 +1,18 @@
-https://www.codewars.com/users/sajjad-salam
+# https://www.codewars.com/users/sajjad-salam
+
+
+
+
+def rental_car_cost(d):
+    cost=40*d
+    if d>=7:
+        cost=cost-50
+      
+    elif d>3:
+        cost=cost-20
+    return cost      
+    
+   
+   
+s=rental_car_cost(4)
+print(s)    

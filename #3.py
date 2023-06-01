8bdc1ac79a2e33db5d96e14007b6df9e48973bbd
sajjad-salam <sajjad.salam.teama@gmail.com>
2023-06-01 12:28:43 +0300

    #3

---
+++ #3.py
@@ -0,0 +1,9 @@
+def simple_multiplication(number) :
+    # Your code goes here
+    if number%2==0:
+      return number*8
+    else:
+      return number*9
+  
+  
+print(simple_multiplication(2))  
